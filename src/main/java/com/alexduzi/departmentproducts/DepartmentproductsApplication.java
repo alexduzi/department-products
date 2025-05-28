ebad2b000909b0982902d4a39e3de6a9c5388e41
alexduzi <duzihd@gmail.com>
2025-05-28 12:35:17 -0300

    first commit

---
+++ src/main/java/com/alexduzi/departmentproducts/DepartmentproductsApplication.java
@@ -0,0 +1,13 @@
+package com.alexduzi.departmentproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexduzi/departmentproducts/DepartmentproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexduzi.departmentproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
