0bff8c73200254c6c82d07ff6abdd5dfca388df8
alexduzi <duzihd@gmail.com>
2025-05-28 16:04:43 -0300

    get department by id

--- src/main/java/com/alexduzi/departmentproducts/controllers/DepartmentController.java
+++ src/main/java/com/alexduzi/departmentproducts/controllers/DepartmentController.java
@@ -4,10 +4,12 @@ import com.alexduzi.departmentproducts.model.dto.DepartmentDTO;
 import com.alexduzi.departmentproducts.services.DepartmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -22,6 +24,12 @@ public class DepartmentController {
     @GetMapping
     public ResponseEntity<List<DepartmentDTO>> findAll() {
         List<DepartmentDTO> result = departmentService.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id) {
+        DepartmentDTO result = departmentService.findById(id);
         return ResponseEntity.ok().body(result);
     }
 }
--- src/main/java/com/alexduzi/departmentproducts/services/DepartmentService.java
+++ src/main/java/com/alexduzi/departmentproducts/services/DepartmentService.java
@@ -2,9 +2,11 @@ package com.alexduzi.departmentproducts.services;
 
 import com.alexduzi.departmentproducts.model.dto.DepartmentDTO;
 import com.alexduzi.departmentproducts.repositories.DepartmentRepository;
+import com.alexduzi.departmentproducts.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,5 +20,9 @@ public class DepartmentService {
 
     public List<DepartmentDTO> findAll() {
         return departmentRepository.findAll().stream().map(DepartmentDTO::new).collect(Collectors.toList());
+    }
+
+    public DepartmentDTO findById(UUID id) {
+        return departmentRepository.findById(id).map(DepartmentDTO::new).orElseThrow(() -> new ResourceNotFoundException("Department not found!"));
     }
 }
